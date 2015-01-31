bb6cdea3ca519bebd8b6c3dcba3fc0be9b6b4483
electro7 <electroseven@gmail.com>
2015-01-31 10:06:44 +0100

    weechat pfff

--- bin/getfocus.sh
+++ bin/getfocus.sh
@@ -5,7 +5,7 @@ id=$(xprop -root | awk '/_NET_ACTIVE_WINDOW\(WINDOW\)/{print $NF}')
 name=$(xprop -id $id | awk '/_NET_WM_NAME/{$1=$2="";print}' | cut -d'"' -f2)
 size=$(echo "$name" | wc -m) 
 if [ $size -gt 40 ]; then
-  name=$(echo "$name" | cut -c 1-40)
+  name=$(echo "$name" | cut -c 1-35)
   name=$name...
 fi
 echo "$name"
