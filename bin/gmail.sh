6a9e465cc605d955329d4575c9f7b950b210b189
electro7 <electroseven@gmail.com>
2015-02-05 12:45:46 +0100

    Account password in safe place

---
+++ bin/gmail.sh
@@ -0,0 +1,27 @@
+
+#!/bin/bash
+
+# Script para ver la cantidad de correos de Gmail.
+# Imprime la cantidad o "err" en caso de algún error
+# -c -> Guarda resultado en ~/.gmail (para conky)
+# 12 ene 2015
+
+# Ruta a los password
+source ~/.private/accounts
+
+MAIL=`curl -u $MAIL_USER:$MAIL_PASS --silent https://mail.google.com/mail/feed/atom`
+CON=`echo $MAIL | grep -c "<fullcount>"`
+NUM=`echo $MAIL | grep -o "<entry>" | wc -l`
+RES="err"
+
+if [ $CON -ne 1 ]; then
+  RES="err"
+else
+  RES="$NUM"
+fi
+
+if [ "$1" = "-c" ]; then
+  echo "$RES" > ~/.gmail
+else
+  echo $RES
+fi
