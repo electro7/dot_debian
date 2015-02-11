7c7c17fc456e66557e1f61ca98ba1e0d44649126
electro7 <electroseven@gmail.com>
2015-02-11 19:41:12 +0100

    bar rules\!

--- .i3/bar/i3_bar.sh
+++ .i3/bar/i3_bar.sh
@@ -12,10 +12,24 @@ trap 'trap - TERM; kill 0' INT TERM QUIT EXIT
 [ -e "${panel_fifo}" ] && rm "${panel_fifo}"
 mkfifo "${panel_fifo}"
 
-# Window title
-xprop -spy -root _NET_ACTIVE_WINDOW | sed -un 's/.*\(0x.*\)/A\1/p' > "$panel_fifo" &
+# Window title, "W"
+xprop -spy -root _NET_ACTIVE_WINDOW | sed -un 's/.*\(0x.*\)/WIN\1/p' > "${panel_fifo}" &
 
-#conky -c $(dirname $0)/i3_bar_conkyrc > "${panel_fifo}" &
+# Volume, "V"
+while :; do
+  amixer get Master | grep Left | awk -F'[]%[]' '/%/ {if ($5 == "off") {print "VOL×\n"} else {printf "VOL%d\n", $2}}' > "${panel_fifo}" &
+  sleep 3s;
+done &
+
+# Datetime, "D", "T"
+while :; do
+  printf "%s%s\n" "TIM" "$(date '+%H:%M')" > "${panel_fifo}"
+  printf "%s%s\n" "DAT" "$(date '+%a %d %b')" > "${panel_fifo}"
+  sleep 30s;
+done &
+
+# Conky
+conky -c $(dirname $0)/i3_bar_conky > "${panel_fifo}" &
 
 $(dirname $0)/i3_bar_parser.sh < "${panel_fifo}" \
   | bar -p -f "${font}" -g "${geometry}" -B "${color_back}" -F "${color_fore}" \
--- .i3/bar/i3_bar_parser.sh
+++ .i3/bar/i3_bar_parser.sh
@@ -1,23 +1,34 @@
 #! /bin/bash
 #
 # bar input parser for bspwm  Tuesday, 01 July 2014 22:57
-
-#screen_width=$(sres -W)
+# 
 
 . $(dirname $0)/i3_bar_config
 
 while read -r line ; do
     case $line in
-        S*)
-            # conky
-            sys_infos="${line#?}"
-            ;;
-        A*)
+        DAT*)
+          # Date
+          date="%{F${color_sec_b1}}%{F${color_green} B${color_sec_b1}} %{T2}Õ%{F- T-} ${line#???} "
+          ;;
+        TIM*)
+          # Time
+          time="%{F${color_lgreen}}%{F${color_back} B${color_lgreen}} ${line#???} %{F- B-}"
+          ;;
+        SYS*)
+          # conky
+          sys_info="${line#???}"
+          ;;
+        VOL*)
+          # Volume
+          vol="%{F${color_sec_b2}}%{F${color_green} B${color_sec_b2}} %{T2}Ô%{F- T-} ${line#???}%% "
+          ;;
+        WIN*)
             # window title
-            name=$(xprop -id ${line#?} | awk '/_NET_WM_NAME/{$1=$2="";print}' | cut -d'"' -f2)
-            title="%{F${color_back} B${color_lgreen}} %{T2}Â%{R}Ú%{F- B- T-}${name}"
+            name=$(xprop -id ${line#???} | awk '/_NET_WM_NAME/{$1=$2="";print}' | cut -d'"' -f2)
+            title="%{F${color_back} B${color_lgreen}}%{T2}Â%{R}Ú%{F- B- T-}${name}"
             ;;
     esac
-    printf "%s\n" "%{%{l}${title} %{r}$sys_infos%{B-}"
+    printf "%s\n" "%{l}${title} %{r}${sys_info}${vol}${date}${time}"
     #printf "%s\n" "%{l B${color_sec_b1}}$wm_infos %{F${color_fore} B-}%{F-} %{c}${title} %{r}$sys_infos%{B-}"
 done
