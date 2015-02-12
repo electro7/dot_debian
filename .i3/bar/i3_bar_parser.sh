612868418aec174d323ff2345e013ebae7f2830c
electro7 <electroseven@gmail.com>
2015-02-12 19:24:58 +0100

    i3_bar alternative colors

--- .i3/bar/i3_bar_parser.sh
+++ .i3/bar/i3_bar_parser.sh
@@ -10,34 +10,29 @@ initr="%{F${color_lgreen}}%{B${color_lgreen}}%{F-}"
 
 while read -r line ; do
     case $line in
-        DAT*)
-          # Date
-          date="%{F${color_sec_b1}}%{F${color_green} B${color_sec_b1}} %{T2}Õ%{F- T-} ${line#???} "
-          ;;
         TIM*)
           # Time
           time="%{F${color_lgreen}}%{F${color_back} B${color_lgreen}} ${line#???} %{F- B-}"
+          ;;
+        DAT*)
+          # Date
+          date="%{F${color_sec_b1}}%{F${color_lgray} B${color_sec_b1}} %{T2}Õ%{F- T-} ${line#???} "
+          ;;
+        VOL*)
+          # Volume
+          vol="%{F${color_sec_b2}}%{F${color_lgray} B${color_sec_b2}} %{T2}Ô%{F- T-} ${line#???}%%  "
           ;;
         SYS*)
           # conky
           sys_info="${line#???}"
           ;;
-        VOL*)
-          # Volume
-          vol="%{F${color_sec_b2}}%{F${color_green} B${color_sec_b2}} %{T2}Ô%{F- T-} ${line#???}%% "
-          ;;
-        WIN*)
-            # window title
-            name=$(xprop -id ${line#???} | awk '/_NET_WM_NAME/{$1=$2="";print}' | cut -d'"' -f2)
-            title="%{F${color_back} B${color_lgreen}}%{T2}Â%{R}Ú%{F- B- T-}${name}"
-            ;;
         GMA*)
           # Gmail
           n_mail="${line#???}"
           if [ "${n_mail}" != "0" ]; then
-            gmail="%{F${color_yellow}}%{F${color_back} B${color_yellow}} %{T2}Ó%{T-} ${n_mail} "
+            gmail="%{F${color_yellow}}%{F${color_back} B${color_yellow}} %{T2}Ó%{T-} ${n_mail}   "
           else
-            gmail="%{F${color_sec_b2}}%{F${color_green} B${color_sec_b2}} %{T2}Ó%{F- T-} ${n_mail} "
+            gmail="%{F${color_sec_b1}}%{F${color_lgray} B${color_sec_b1}} %{T2}Ó%{F- T-} ${n_mail}   "
           fi
           ;;
         IRC*)
@@ -45,14 +40,19 @@ while read -r line ; do
           if [ "${line#???}" != "0" ]; then
             ((irc_n_high++))
             irc_high="${line#???}"
-            irc="%{F${color_lred}}%{F${color_back} B${color_lred}} %{T2}Ò%{T-} ${irc_n_high}  %{T2}Á%{T-} ${irc_high} "
+            irc="%{F${color_lred}}%{F${color_back} B${color_lred}} %{T2}Ò%{T-} ${irc_n_high}  %{T2}Á%{T-} ${irc_high}  "
           else
             irc_n_high=0
             [ -z "${irc_high}" ] && irc_high="none"
-            irc="%{F${color_sec_b1}}%{F${color_green} B${color_sec_b1}} %{T2}Ò%{F- T-} ${irc_n_high} %{F${color_green}} %{T2}Á%{F- T-} ${irc_high} "
+            irc="%{F${color_sec_b2}}%{F${color_lgray} B${color_sec_b2}} %{T2}Ò%{F- T-} ${irc_n_high} %{F${color_lgray}} %{T2}Á%{F- T-} ${irc_high}  "
           fi
           ;;
+        WIN*)
+          # window title
+          name=$(xprop -id ${line#???} | awk '/_NET_WM_NAME/{$1=$2="";print}' | cut -d'"' -f2)
+          title="%{F${color_back} B${color_lgreen}}%{T2}Â%{R}Ú%{F- B- T-}${name}"
+          ;;
     esac
-    printf "%s\n" "%{l}${title} %{r}${initr}${irc}${gmail}${sys_info}${vol}${date}${time}"
+    printf "%s\n" "%{l}${title} %{r}${irc}${gmail}${sys_info}${vol}${date}${time}"
     #printf "%s\n" "%{l B${color_sec_b1}}$wm_infos %{F${color_fore} B-}%{F-} %{c}${title} %{r}$sys_infos%{B-}"
 done
