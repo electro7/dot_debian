60eda07819fa5572a0b946128caf26b99cf133ca
electro7 <electroseven@gmail.com>
2015-08-20 12:19:34 +0200

    Only one every for all interval update meters

--- .i3/lemonbar/i3_lemonbar.sh
+++ .i3/lemonbar/i3_lemonbar.sh
@@ -14,6 +14,8 @@ trap 'trap - TERM; kill 0' INT TERM QUIT EXIT
 [ -e "${panel_fifo}" ] && rm "${panel_fifo}"
 mkfifo "${panel_fifo}"
 
+### EVENTS METERS
+
 # Window title, "WIN"
 xprop -spy -root _NET_ACTIVE_WINDOW | sed -un 's/.*\(0x.*\)/WIN\1/p' > "${panel_fifo}" &
 
@@ -22,25 +24,6 @@ xprop -spy -root _NET_ACTIVE_WINDOW | sed -un 's/.*\(0x.*\)/WIN\1/p' > "${panel_
 #$(dirname $0)/i3_workspaces.py > "${panel_fifo}" &
 $(dirname $0)/i3_workspaces.pl > "${panel_fifo}" &
 
-# Volume, "VOL"
-while :; do
-  amixer get Master | grep "${snd_cha}" | awk -F'[]%[]' '/%/ {if ($7 == "off") {print "VOL×\n"} else {printf "VOL%d%%%%\n", $2}}' > "${panel_fifo}" &
-  sleep 3s;
-done &
-
-# GMAIL, "GMA"
-while :; do
-  printf "%s%s\n" "GMA" "$(~/bin/gmail.sh)" > "${panel_fifo}"
-  sleep 300s;
-done &
-
-#MPD
-while :; do
-  #printf "%s%s\n" "MPD" "$(ncmpcpp --now-playing '{%a - %t}|{%f}' | head -c 60)" > "${panel_fifo}"
-  printf "%s%s\n" "MPD" "$(mpc current -f '[[%artist% - ]%title%]|[%file%]' 2>&1 | head -c 70)" > "${panel_fifo}"
-  sleep 10s;
-done &
-
 # IRC, "IRC"
 # only for init
 ~/bin/irc_warn &
@@ -48,7 +31,39 @@ done &
 # Conky, "SYS"
 conky -c $(dirname $0)/i3_lemonbar_conky > "${panel_fifo}" &
 
-# Loop fifo
+### UPDATE INTERVAL METERS
+cnt_vol=${upd_vol}
+cnt_mail=${upd_mail}
+cnt_mpd=${upd_mpd}
+
+while :; do
+
+  # Volume, "VOL"
+  if [ $((cnt_vol++)) -ge ${upd_vol} ]; then
+    amixer get Master | grep "${snd_cha}" | awk -F'[]%[]' '/%/ {if ($7 == "off") {print "VOL×\n"} else {printf "VOL%d%%%%\n", $2}}' > "${panel_fifo}" &
+    cnt_vol=0
+  fi
+
+  # GMAIL, "GMA"
+  if [ $((cnt_mail++)) -ge ${upd_mail} ]; then
+    printf "%s%s\n" "GMA" "$(~/bin/gmail.sh)" > "${panel_fifo}"
+    cnt_mail=0
+  fi
+
+  # MPD
+  if [ $((cnt_mpd++)) -ge ${upd_mpd} ]; then
+    #printf "%s%s\n" "MPD" "$(ncmpcpp --now-playing '{%a - %t}|{%f}' | head -c 60)" > "${panel_fifo}"
+    printf "%s%s\n" "MPD" "$(mpc current -f '[[%artist% - ]%title%]|[%file%]' 2>&1 | head -c 70)" > "${panel_fifo}"
+    cnt_mpd=0
+  fi
+
+  # Finally, wait 1 second
+  sleep 1s;
+
+done &
+
+#### LOOP FIFO
+
 $(dirname $0)/i3_lemonbar_parser.sh < "${panel_fifo}" \
   | lemonbar -p -f "${font}" -f "${iconfont}" -g "${geometry}" -B "${color_back}" -F "${color_fore}" \
   | while read line; do eval "$line"; done &
