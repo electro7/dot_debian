e27a3ed380ce6a1f2c37021e0ab72a8e01984e13
electro7 <electroseven@gmail.com>
2015-10-24 11:22:25 -0500

    changed netisntall to own form of repo

--- netinstall/install2.sh
+++ netinstall/install2.sh
@@ -1,6 +1,6 @@
 cd ~
 find . -delete
-git clone https://github.com/electro7/dotfiles.git .
+git clone https://github.com/mope1/dotfiles.git .
 git clone https://github.com/gmarik/Vundle.git ~/.vim/bundle/Vundle.vim
 vim +PluginInstall +qall
 
