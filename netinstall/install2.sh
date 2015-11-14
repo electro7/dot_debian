0a94a4b94bad7e8d90adb7be70d6a4b088be3112
electro7 <electroseven@gmail.com>
2015-11-14 10:32:02 -0600

    changed home directory clearing to config file removal

--- netinstall/install2.sh
+++ netinstall/install2.sh
@@ -1,6 +1,11 @@
 cd ~
-find . -delete
-git clone https://github.com/mope1/dotfiles.git .
+mkdir Documents
+mkdir Documents/dotfiles
+git clone https://github.com/mope1/dotfiles.git Documents/dotfiles
+for conf in $(ls Documents/dotfiles |grep -Ev '^\.\.?\/'); do
+	rm -rfv $conf
+done
+cp -rfv Documents/dotfiles/* .
 git clone https://github.com/gmarik/Vundle.git ~/.vim/bundle/Vundle.vim
 vim +PluginInstall +qall
 
